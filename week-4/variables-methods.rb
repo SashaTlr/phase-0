61ed0c6b4fd239c0b4ebd7e0508e0a8fbd4e665d
Sasha Tlr <sashatailor@gmail.com>
2015-10-21 22:43:56 -0400

    Add reflection for 4.3

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -22,3 +22,25 @@ favorite_number = gets.chomp
 new_number = favorite_number.to_i + 1
 
 p new_number.to_s + ' is a bigger and better number!'
+
+=begin
+
+How do you define a local variable?
+  A local variable whose scope is limited to its parent' function. Calling the variable outside of the function will not work unless the variable has also been defined outside of the function.
+
+How do you define a method?
+  A method in Ruby is a function that is defined on an object, since Ruby is an object-oriented language.
+
+What is the difference between a local variable and a method?
+  A method is a function, i.e. it is an action that can result in a value or string, while a variable is a container to hold some sort of value or string.
+
+How do you run a ruby program from the command line?
+  Assuming ruby is already installed, run ruby <filename>, where the filename is a ruby file (.rb) containing the program.
+
+How do you run an RSpec file from the command line?
+To run a test file, reference the program in the spec file, and then use command: rspec <spec file name> where the spec file (.spec) contains a series of tests.
+
+What was confusing about this material? What made sense?
+I'm not used to working without a compiler, so the Rspec testing protocols were a bit confusing.
+
+=end
