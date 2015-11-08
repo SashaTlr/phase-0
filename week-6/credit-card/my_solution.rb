ccefe8c859de17dd6ef95c9be3f3644b8cd21ab5
Sasha Tlr <sashatailor@gmail.com>
2015-11-08 16:53:03 -0500

    Add 6.7 reflection and fixes

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -1,8 +1,8 @@
 # Class Warfare, Validate a Credit Card Number
 
 
-# I worked on this challenge [by myself, with: ].
-# I spent [#] hours on this challenge.
+# I worked on this challenge with Peter.
+# I spent 3 hours on this challenge.
 # Pseudocode
 
 # Input: Integer
@@ -45,13 +45,13 @@
 
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
-
+=begin
 class CreditCard
   def initialize ( userNum )
     if userNum.to_s.size < 16
-      raise ArgumentError( 'Card number not long enough!' )
+      raise ArgumentError.new( 'Card number not long enough!' )
     elsif userNum.to_s.size > 16
-      raise ArgumentError( 'Card number too long!' )
+      raise ArgumentError.new( 'Card number too long!' )
     else
       remainder = userNum
       @cardNum = Array.new( 16 )
@@ -98,17 +98,14 @@ class CreditCard
   end
 end
 
-
-mycard = CreditCard.new( 4563960122001999 )
-mycard.debug
-
+=end
 # Refactored Solution
 class CreditCard
   def initialize ( userNum )
     if userNum.to_s.size < 16
-      raise ArgumentError( 'Card number not long enough!' )
+      raise ArgumentError.new( 'Card number not long enough!' )
     elsif userNum.to_s.size > 16
-      raise ArgumentError( 'Card number too long!' )
+      raise ArgumentError.new( 'Card number too long!' )
     else
       @cardNum = userNum.to_s.split('').map{|i| i.to_i}
     end
@@ -126,7 +123,7 @@ class CreditCard
       }
   end
 
-  def check_card?
+  def check_card
     array_sum % 10 == 0 ? true : false
   end
 
@@ -139,13 +136,12 @@ class CreditCard
 end
 
 
-mycard = CreditCard.new( 4563960122001999 )
-mycard.debug
-
-
-
-
-
-
-
 # Reflection
+#What was the most difficult part of this challenge for you and your pair?
+# => We have very different work styles, and I think we clashed a bit during our pairing session. On my pair's end, I think he was stressed out with work and was focused on plowing through without realizing that he was cutting me out, and I think he had the impression that I wasn't very experienced, so didn't want to slow down for me although I was totally on top of what was going on.
+# => We ended up taking much longer than necessary because we weren't in agreement on implementations, and I cancelled our second pairing session because I wasn't treated with respect for the first session and didn't want to be treated like that a second time. Fortunately, I sent him an email briefly explaining the situation and he came back with a very positive note, so we were able to clear the air quickly instead of railing on each other in our feedback and holding a grudge. This was a good learning experience for me, and validated what I assumed, that even though some people may underestimate my capabilities, possibly due to certain biases or stereotypes, they are not inherently bad people and often don't recognize their own assumptions. Starting a dialogue is more effective than getting mad and reciprocating the treatment.
+
+#What new methods did you find to help you when you refactored?
+# => Didn't implement any new methods but combined each_index and map together in a way that I hadn't used previously.
+#What concepts or learnings were you able to solidify in this challenge?
+#Whathile I have used the inject method before, I have usually used it to take a cumulative sum. In this method, I had to use inject(0) because my first sum couldn't simply equal the first element of the array, as there was some arithmetic manipulation that had to occur on the element first. Using the inject(0) method in this context finally clarified the purpose of inject(0), as I struggled a lot with this function when I was learning it on codeacademy.
